39bd5cc2decc07d1f34810864c1de49159b6d614
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-03 14:41:20 +0200

    bonus n.5 use property to convert a string to uppercase letters

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -25,7 +25,7 @@ console.log(cityReplaced); // Output:  "New Delhi"
 const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
-// 5. Tutto in maiscolo
-// Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = '';
+// 5. Tutto in maiuscolo
+// Trasforma tutto in maiuscolo la stringa della variabile hello
+const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
