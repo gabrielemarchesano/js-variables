d7601a572e2edb07e5010e5bf6c6cde7e1733094
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-03 13:56:52 +0200

    set n.2 instruction of the 2nd folder by adding number, text and boolean variables and print all of them

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,11 @@ console.log(myName);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 10;
+const text = "Ciao tutor"
+const bool = true;
+console.log(number, text, bool);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
