cb946af78c03a442f509a144ee5dc3c933c558a2
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-03 16:01:47 +0200

    bonus n.1 correction of the search filter parameter

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,8 +6,7 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = city.search(/y/i); 
-/* Ho usato /y/ per cercare la lettera y e la i indica il case insensitive, cioè trova la y anche se è scritta in maiuscolo */
+const cityIndex = city.search("Y");
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
